1e0c77268cd82eaf8140632355ad41de4978b295
Sue Myers <smyers@boxstep.org>
2020-05-24 12:56:40 -0400

    net-iteration

--- Iterations.cs
+++ Iterations.cs
@@ -57,21 +57,18 @@ namespace DotnetIteration
         // 
         public static IEnumerable<string> StringyIndexes(List<string> data)
         {
-            var newIndexList = data.Select((word) => ("is at index {data.IndexOf}(", "));
-
+            var newIndexList = data.Select((dataItem, selectIndex) => $"{dataItem} is at index {selectIndex}");
+            return newIndexList;
         }
-
-
         // 
         // 4) Complete the method OnlyTheEvenSurvive that accepts an list of
         //    numbers and returns only the elements that are even.
         // 
         public static IEnumerable<int> OnlyTheEvenSurvive(List<int> data)
         {
-            throw new System.NotImplementedException();
+            var evenData = new List<int> { 42, 50, 100, 5, -43, 17, 44 }.Where(data => data % 2 == 0);
+            return evenData;
         }
-
-
         // 
         // 5) Complete the method OnlyTheEvenIndexedSurvive that accepts an
         //    list of numbers and returns only the elements at indexes that
@@ -79,7 +76,10 @@ namespace DotnetIteration
         // 
         public static IEnumerable<int> OnlyTheEvenIndexedSurvive(List<int> data)
         {
-            throw new System.NotImplementedException();
+            //var dataAtEvenIndex = new List<int>{ 31, 67, 64, 96, 14, 24, 43, 51, 48, 80, 58, 43, 64, 84, 98, 99, 69, 93, 5, 32, 29, 4, 28, 18, 86, 22, 20, 74, 35, 27, 85, 79, 65, 32, 56, 94, 93, 20, 29, 22, 72 };
+
+            var everySecond = new List<int> { 31, 67, 64, 96, 14, 24, 43, 51, 48, 80, 58, 43, 64, 84, 98, 99, 69, 93, 5, 32, 29, 4, 28, 18, 86, 22, 20, 74, 35, 27, 85, 79, 65, 32, 56, 94, 93, 20, 29, 22, 72 }.Where((data, element) => element % 2 == 0);
+            return everySecond;
         }
 
 
@@ -96,9 +96,15 @@ namespace DotnetIteration
         //   score: 99
         // }
         // 
-        public static IEnumerable<string> BestMovieOfTheYear(List<Movie> data, int year)
+        public static IEnumerable<string> BestMovieOfTheYear(List<Movie> movies, int year)
         {
-            throw new System.NotImplementedException();
+            //get all movies with scores greater than 90
+            //of those with scores > 90, list by year 2014 as a variable , then do by 17 as another variable
+            //then combine lists as a master list
+
+            var bestMovieOfTheYear = movies.Where(movie => movie.Score > 90 && movie.Year == year);
+            var nameOfBestMovieOfTheYear = bestMovieOfTheYear.Select(movie => movie.Name);
+            return nameOfBestMovieOfTheYear;
         }
 
 
@@ -108,7 +114,9 @@ namespace DotnetIteration
         // 
         public static bool EveryoneIsOdd(List<int> data)
         {
-            throw new System.NotImplementedException();
+
+            var areAllIntegersOdd = data.All(dataItem => dataItem % 2 > 0);
+            return areAllIntegersOdd;
         }
 
 
